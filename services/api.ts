a3f18dafe195f077e919a59af334b1024b974441
gson Liang <yuanyu90221@gmail.com>
2024-10-02 21:43:11 +0800

    feat: modify api reference
    modify api reference
    
    modify api reference

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { Platform } from 'react-native';
 
-const url = Platform.OS === 'android' ? 'https://55ff-2001-b400-e240-2ac4-5fa-a26c-8c43-5580.ngrok-free.app':'http://127.0.0.1:3000';
+const url = Platform.OS === 'android' ? 'http://10.0.2.2:3000':'http://127.0.0.1:3000';
 
 const Api: AxiosInstance = axios.create({baseURL: url });
 Api.interceptors.request.use(async config =>{
